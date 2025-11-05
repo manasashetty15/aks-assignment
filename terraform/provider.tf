1720637bc7e5165e4a1702aebd1efa51d928b113
manasashetty15 <manasa.shetty15@gmail.com>
2025-11-05 21:29:11 +0530

    Trigger CI/CD pipeline

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,13 @@
 provider "azurerm" {
   features {}
+  subscription_id = "aff1baf1-24fc-4c01-a0fe-f510e62915b0"
 }
 
 terraform {
   required_version = ">= 1.3.0"
   backend "azurerm" {
-    resource_group_name  = "<storage-rg>"
-    storage_account_name = "<storage-account>"
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "aksstorageaccount3040"
     container_name       = "tfstate"
     key                  = "aks.tfstate"
   }
